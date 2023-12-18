882195e1a47cd98c5a095f8785cfbd60b164683e
Omonjon <omonjons077@gmail.com>
2023-12-18 22:17:19 +0500

    Site home page done 100%

---
+++ assets/script/main.js
@@ -0,0 +1,35 @@
+'use strict'
+/*=============== LOADER ===============*/
+document.addEventListener('DOMContentLoaded', function (eventObject) {
+    $('.load').fadeIn();
+})
+window.addEventListener("load", function (eventObject) {
+    $('.load').fadeOut("slow");
+});
+
+/*=============== Header Fixed ===============*/
+if ($("#myHeader").length) {
+    window.onscroll = function () {
+        myFunction()
+    };
+
+    let header = document.getElementById("myHeader");
+    let sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+}
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 300 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 300 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
